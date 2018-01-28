25e6c4c20ed03f0536cf789b4fdb893a412bd3b6
GBlodgett35 <GBlod@DESKTOP-PG7AFS2.ZoomTown.com>
2018-01-28 15:31:27 -0500

    Added some print statements so the user could see whether the specific numbers are prime or not.

--- Assignment03_Spring2018/src/assignment03Spring2018/BigIntPrime.java
+++ Assignment03_Spring2018/src/assignment03Spring2018/BigIntPrime.java
@@ -5,7 +5,21 @@ import java.math.BigInteger;
 public class BigIntPrime {
 	public boolean isPrime(BigInteger number)
 	{
-		boolean result = true;
+		boolean result = true;// Assume number is prime until we prove otherwise 
+		if (number.equals(BigInteger.ONE)) {return false;}		// One is not prime AND not non-prime. How should we handle this? 
+		BigInteger divisor = new BigInteger("2"); 
+		BigInteger limit = number.divide(new BigInteger("2"));
+		limit = limit.add(BigInteger.ONE); 
+		// Loop until the divisor is >= half of the original number 
+		while (true) { 
+			if (limit.equals(divisor)) {break;}		// We're done
+		 
+			if (number.mod(divisor).equals(BigInteger.ZERO)) {	// Does divisor divide evenly into our number? If so, we're not prime. Time to quit. 
+				result = false;
+				break;
+			}
+			divisor = divisor.add(BigInteger.ONE); 
+		} 
 		return result;
 	}
 
--- Assignment03_Spring2018/src/assignment03Spring2018/Main.java
+++ Assignment03_Spring2018/src/assignment03Spring2018/Main.java
@@ -20,27 +20,38 @@ public class Main {
 		//Expected: false
 		BigIntPrime test1 = new BigIntPrime();
 		boolean result1 = test1.isPrime(BigInteger.valueOf(1));
+		//Print out the results
+		if(result1==true) {System.out.println("1 is prime");}
+		else {System.out.println("1 is not prime");}
 		//Test whether 2 is prime
 		//Expected: true
 		BigIntPrime test2 = new BigIntPrime();
 		boolean result2 = test2.isPrime(BigInteger.valueOf(2));
+		if(result2==true) {System.out.println("2 is prime");}
+		else {System.out.println("2 is not prime");}
 		//Test whether 7700000070077 is prime
 		//Expected: False
 		BigIntPrime test3 = new BigIntPrime();
 		boolean result3 = test3.isPrime(BigInteger.valueOf(7700000070077L));
+		if(result3==true) {System.out.println("7700000070077 is prime");}
+		else {System.out.println("7700000070077 is not prime");}
 		//Test whether 179426549 is prime
 		//Expected: true
 		BigIntPrime test4 = new BigIntPrime();
 		boolean result4 = test4.isPrime(BigInteger.valueOf(179426549));
+		if(result4==true) {System.out.println("179426549 is prime");}
+		else {System.out.println("179426549 is not prime");}
 		//Check whether 1000000000000000000 is prime
 		//Expected: false
 		BigIntPrime test5 = new BigIntPrime();
 		boolean result5 = test5.isPrime(BigInteger.valueOf(1000000000000000000L));
-		//Check if all the test cases turn out as expected.
+		if(result5==true) {System.out.println("1000000000000000000 is prime");}
+		else { System.out.println("1000000000000000000 is not prime");}
+		//Check if all the test cases turn out as expected.				
 		if(result1==false && result2==true && result3== false && result4==true && result5==false)
 		{
-			//If it is print out a message saying so
-			System.out.println("Your program works!!");
+		//If it is print out a message saying so
+		System.out.println("Your program works!!");
 		}
 		else
 		{
